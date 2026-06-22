7fb85f138226ac506a0448305dd80083716fda73
palakpreet <kaurpalakpreet61@gmail.com>
2026-06-22 04:01:18 +0000

    Deployment 2026-06-22_04-01-18

---
+++ scripts/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+
+echo "Deployment Started $DATE" >> ../logs/deploy.log
+
+echo "Creating Backup..."
+
+rm -rf ../backup/*
+
+cp -r ../website/* ../backup/
+
+echo "Backup Completed" >> ../logs/deploy.log
+
+echo "Pushing to GitHub..."
+
+cd ..
+
+git add .
+
+git commit -m "Deployment $DATE"
+
+git push origin main
+
+echo "Deploying to S3..."
+
+aws s3 sync website/ s3://company-website-12345
+
+echo "Deployment Completed $DATE" >> logs/deploy.log
+
+echo "Done"
